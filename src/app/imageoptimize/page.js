0d5b8e8dc74de1f2463f89b6e49a08ee5b13ec7f
Akib Ali <akib7599@gmail.com>
2024-11-13 06:48:44 -0800

    fix image issue

--- src/app/imageoptimize/page.js
+++ src/app/imageoptimize/page.js
@@ -8,8 +8,8 @@ const ImageOptimize=()=>{
     return(
         <>
          <h1>Here Learn Image Optimize</h1>
-         <Image src={logo}/>
-         <img src={logo.src} />
+         <Image src={logo} alt="image-3"/>
+         <img src={logo.src} alt="img-4" />
          <Image src="https://plus.unsplash.com/premium_photo-1667480556784-a8f27e62104c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGR1bW15fGVufDB8fDB8fHww" height={200} width={200} alt="dummy img"/>
          
         </>
